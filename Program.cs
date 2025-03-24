e1bea6ba2c9f5392412f7004d3c3d88be0909d2e
Erik <eriksabino2030@gmail.com>
2025-03-24 10:46:15 -0300

    Programa Finalizado

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Digite seu nome: ");
+string nome = Console.ReadLine()!;
+
+Console.Write("Digite seu sobrenome: ");
+string sobrenome = Console.ReadLine()!;
+Console.WriteLine($"Seu nome completo é: {nome} {sobrenome}");
+Console.Write($"Seu nome catálogo é: {sobrenome.ToUpper()}, {nome}");
+
